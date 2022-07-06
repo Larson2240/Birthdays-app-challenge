da2592bfbc33f6c70d8fc4297b7c4073950ef579
Seraphim Kovalchuk <seraphimkovalchuk@gmail.com>
2022-07-06 16:02:07 +0200

    Created new project. Initial commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,20 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Seraphim Kovalchuk on 06.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
